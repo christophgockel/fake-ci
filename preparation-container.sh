bd44bec9fdc07d8f00545e5c448f6bba595cc62d
Christoph Gockel <git@christophgockel.de>
2022-10-23 20:53:31 +0100

    Updates Preparation Container to read required artifacts from YAML file

--- preparation-container.sh
+++ preparation-container.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 job_name="$1"
 
-# further artifacts and cache steps to be added later
 commands_to_run="
   cp -Rp /checkout/. /job;
 "
@@ -26,12 +25,28 @@ docker exec \
   sh -c "${commands_to_run/$'\n'/}"
 
 # after copying the code get the artifacts in place
-if [ "$job_name" = "test" ]
+# cache steps to be added later
+job_names_with_artifacts=$(yq ".${job_name}.needs[] | select(.artifacts == true) | .job" .gitlab-ci.yml)
+
+if [ -n "$job_names_with_artifacts" ]
 then
   echo "Preparing artifacts."
-  commands_to_run="
-    cp -Rp /artifacts/build/file.txt /job;
-  "
+
+  commands_to_run=""
+
+  while IFS= read -r job_name_of_artifact
+  do
+    artifact_paths=$(yq ".${job_name_of_artifact}.artifacts.paths[]" .gitlab-ci.yml)
+
+    if [ -n "$artifact_paths" ]
+    then
+      while IFS= read -r artifact_path
+      do
+        commands_to_run+="cp -Rp /artifacts/${job_name_of_artifact}/${artifact_path} /job;"
+      done < <(echo "$artifact_paths")
+    fi
+
+  done < <(echo "$job_names_with_artifacts")
 
   docker exec \
     fake-ci-preparation \
