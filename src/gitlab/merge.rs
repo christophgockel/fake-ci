ed027471477f9822807f88159fb88ffbfb6aeff1
Christoph Gockel <git@christophgockel.de>
2022-10-29 12:32:34 +0100

    Adds function to traverse and collect all template names for a job

--- src/gitlab/merge.rs
+++ src/gitlab/merge.rs
@@ -1,4 +1,6 @@
-use crate::gitlab::configuration::ListOfStrings;
+use crate::gitlab::configuration::{Job, ListOfStrings};
+use std::collections::HashMap;
+use std::io::ErrorKind;
 
 pub fn merge_variables(source: &[(String, String)], target: &mut Vec<(String, String)>) {
     target.splice(0..0, source.to_owned());
@@ -14,6 +16,29 @@ pub fn merge_script(source: &Option<ListOfStrings>, target: &mut Option<ListOfSt
     if let (Some(s), t @ None) = (source, target) {
         let _ = t.insert(ListOfStrings(s.0.clone()));
     };
+}
+
+pub fn collect_template_names(
+    job: &Job,
+    all_templates: &HashMap<String, Job>,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut collected_names = vec![];
+
+    if let Some(ListOfStrings(template_names)) = &job.extends {
+        for template_name in template_names {
+            let template = all_templates
+                .get(template_name)
+                .ok_or_else(|| std::io::Error::new(ErrorKind::NotFound, "template not found"))?;
+
+            if template.extends.is_some() {
+                collected_names.extend(collect_template_names(template, all_templates)?);
+            }
+
+            collected_names.push(template_name.to_owned());
+        }
+    }
+
+    Ok(collected_names)
 }
 
 #[cfg(test)]
@@ -105,6 +130,55 @@ mod tests {
             merge_script(&source, &mut target);
 
             assert_eq!(target, Some(ListOfStrings(vec!["value".into()])));
+        }
+    }
+
+    mod test_collecting_templates {
+        use super::*;
+        use crate::gitlab::configuration::Job;
+        use std::collections::HashMap;
+
+        #[test]
+        fn fails_when_template_does_not_exist() {
+            let empty_templates = HashMap::new();
+            let mut job_with_templates = Job::default();
+            job_with_templates.extends = Some(ListOfStrings(vec![".template-name".into()]));
+
+            let result = collect_template_names(&job_with_templates, &empty_templates);
+
+            assert!(result.is_err());
+        }
+
+        #[test]
+        fn collects_template_names_that_are_used_by_job() {
+            let templates = HashMap::from([
+                (".template-a".into(), Job::default()),
+                (".template-b".into(), Job::default()),
+            ]);
+            let mut job_with_templates = Job::default();
+            job_with_templates.extends = Some(ListOfStrings(vec![".template-b".into()]));
+
+            let names = collect_template_names(&job_with_templates, &templates).unwrap();
+
+            assert_eq!(names, vec![".template-b".to_string()])
+        }
+
+        #[test]
+        fn looks_further_into_templates_to_collect_all_their_templates_ordered_by_hierarchy() {
+            let mut template_with_additional_extend = Job::default();
+            template_with_additional_extend.extends = Some(ListOfStrings(vec![".parent".into()]));
+
+            let templates = HashMap::from([
+                (".template-a".into(), Job::default()),
+                (".template-b".into(), template_with_additional_extend),
+                (".parent".into(), Job::default()),
+            ]);
+            let mut job_with_templates = Job::default();
+            job_with_templates.extends = Some(ListOfStrings(vec![".template-b".into()]));
+
+            let names = collect_template_names(&job_with_templates, &templates).unwrap();
+
+            assert_eq!(names, vec![".parent".into(), ".template-b".to_string()])
         }
     }
 }
